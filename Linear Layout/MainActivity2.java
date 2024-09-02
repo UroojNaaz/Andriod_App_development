19a372f3b8d4ddeb4317a5c64c93bb2d399babda
UROOJ NAZ <142103317+UroojNaaz@users.noreply.github.com>
2024-09-02 13:58:06 +0500

    Update MainActivity2.java

--- Linear Layout/MainActivity2.java
+++ Linear Layout/MainActivity2.java
@@ -1,1 +1,101 @@
+<!--task-2 (by using relative and linear layout) -->
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:id="@+id/main"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:accessibilityTraversalAfter="@id/null"
+    android:rotationX="-2"
+    android:padding="10dp"
+    tools:context=".MainActivity">
 
+        <TextView
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="POKEMON APP"
+        android:textStyle="bold"
+        android:textSize="20sp"
+        android:layout_marginTop="10dp"
+        android:textAlignment="center"
+        android:textColor="@color/white"
+        android:background="@color/black"
+        />
+
+
+    <LinearLayout
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:backgroundTint="@color/black"
+        android:layout_marginTop="35dp"
+        android:orientation="horizontal"
+        android:padding="10dp">
+
+        <ImageView
+            android:layout_width="180dp"
+            android:layout_height="200dp"
+            android:layout_margin="6dp"
+            android:scaleType="fitXY"
+            android:src="@drawable/pokemon" />
+
+        <ImageView
+            android:layout_width="180dp"
+            android:layout_height="200dp"
+            android:layout_margin="6dp"
+            android:scaleType="fitXY"
+            android:src="@drawable/pokemon" />
+
+
+    </LinearLayout>
+
+    <LinearLayout
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:orientation="horizontal"
+        android:layout_marginTop="250dp"
+        android:backgroundTint="@color/black"
+        android:padding="10dp">
+
+        <ImageView
+            android:layout_width="180dp"
+            android:layout_height="200dp"
+            android:layout_margin="6dp"
+            android:scaleType="fitXY"
+            android:src="@drawable/pokemon" />
+
+        <ImageView
+            android:layout_width="180dp"
+            android:layout_height="200dp"
+            android:layout_margin="6dp"
+            android:scaleType="fitXY"
+            android:src="@drawable/pokemon" />
+
+    </LinearLayout>
+
+    <LinearLayout
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:backgroundTint="@color/black"
+        android:layout_marginTop="470dp"
+        android:orientation="horizontal"
+        android:padding="10dp">
+
+        <ImageView
+            android:layout_width="180dp"
+            android:layout_height="200dp"
+            android:layout_margin="6dp"
+            android:scaleType="fitXY"
+            android:src="@drawable/pokemon" />
+
+        <ImageView
+            android:layout_width="180dp"
+            android:layout_height="200dp"
+            android:layout_margin="6dp"
+            android:scaleType="fitXY"
+            android:src="@drawable/pokemon" />
+
+    </LinearLayout>
+
+</RelativeLayout>
